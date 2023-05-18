2ed7c4aea76f12511b67654ae9057b18b8f81800
shhhplus <shhhplus@msn.com>
2023-05-18 16:29:26 +0800

    optimize

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  coverageReporters: ['clover', 'json', 'lcov', 'text', 'cobertura'],
+
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
 
